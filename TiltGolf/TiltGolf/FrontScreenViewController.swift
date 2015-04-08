2719b7e7ba1c28c5bd39c9076e45b03b9ca785fd
uics12 <uics12@wireless-nat-inside.uiowa.edu>
2015-04-08 15:09:15 -0500

    add some screens

---
+++ TiltGolf/TiltGolf/FrontScreenViewController.swift
@@ -0,0 +1,22 @@
+//
+//  FrontScreenViewController.swift
+//  TiltGolf
+//
+//  Created by uics12 on 4/8/15.
+//  Copyright (c) 2015 iOSClass. All rights reserved.
+//
+import UIKit
+import Foundation
+class FrontScreenViewController: UIViewController {
+
+    @IBAction func StartButtonPressed() {
+        
+        
+    }
+
+    @IBAction func InstructionsButtonPressed() {
+        
+        
+    }
+
+}
